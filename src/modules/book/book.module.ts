b220814938a718f9a20768ee53cb7697b65cc359
MatheusNachtigall <matheusgnachtigall@gmail.com>
2023-06-02 14:05:22 -0300

    feat: :bug: change prismaService absolute path to relative path

--- src/modules/book/book.module.ts
+++ src/modules/book/book.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { BookService } from './book.service';
 import { BookController } from './book.controller';
-import { PrismaService } from 'src/database/PrismaService';
+import { PrismaService } from '../../database/PrismaService';
 
 @Module({
   controllers: [BookController],
